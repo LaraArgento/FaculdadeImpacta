f2b72a4ccb95b0774b66617d0b88bee34a5934c4
Lara Argento <lara.agnt@gmail.com>
2020-10-07 23:52:55 -0300

    publisher att

--- 4semestre/3 Desenvolvimento para Internet das Coisas - IoT/Aula08/AtividadeContinua03/publisher.py
+++ 4semestre/3 Desenvolvimento para Internet das Coisas - IoT/Aula08/AtividadeContinua03/publisher.py
@@ -10,8 +10,7 @@ client = mqtt.Client()
 client.connect("mqtt.eclipse.org", 1883, 60)
 
 # codificando o payload
-#st =  "\n\nArcheros 1900519 - Fernando de Souza Franco \n 1901118 - Lara Angelini Argento \n 1901008 - Leonardo Ferreira \n 1900675 - Lucas Eduardo Ano \n 1900953 - Nicholas Ferreira"   
-st = "\n _/﹋\_ \n (҂`_´) \n <,︻╦╤─ ҉ - - \n _/﹋\_\n"
+st =  "calor dmai"
 payload = st.encode()
 # envia a publicação
 client.publish(TOPICO,payload,qos=0)
